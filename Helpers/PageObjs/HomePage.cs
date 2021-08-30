bcc8823606bc40eeab24ab21fcc40c4ca72b7158
Raymond Dasilva <rdasilva@fieldedge.com>
2021-08-30 16:30:48 -0400

    Added Parallel-browser test runs. (Chrome / FireFox)

---
+++ Helpers/PageObjs/HomePage.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Safari;
+using System;
+using System.Threading.Tasks;
+using WebAutomation.Helpers;
+
+namespace WebAutomation.Helpers.PageObjs
+{
+    public class HomePage : ElementUtility
+    {
+        private string affirmHomePage = "https://www.affirm.com/";
+        private string loginButton = "#gatsby-focus-wrapper > div.Header-outer--NMN0k > div > div > div.Container-container--3LGAe.Header-wrapper--rEEKq.Header-hideOnSmartphone--3LTpT > a:nth-child(4) > span";
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {            
+            this.driver = driver;
+        }        
+
+        #region Homepage functions
+
+        /// <summary>
+        /// Goes to Affirm's HomePage and wait until an element is visible, confirming it's loaded.
+        /// </summary>
+        /// <returns>A boolean indicating success</returns>      
+        public async Task<bool> GoToHomePage()
+        {
+            try
+            {
+                // Navigate to Homepage
+                driver.Navigate().GoToUrl(affirmHomePage);
+                driver.Manage().Window.Maximize();
+                await PauseAsync(300);
+
+                // Make sure there's a button visible before we assume the page has loaded.
+                GetElement(driver, By.CssSelector(loginButton), 15);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }                     
+        }                
+
+        #endregion Homepage functions
+    }
+}
--- WebTests/Selenium/TestCases/StaticTests/Login.cs
+++ WebTests/Selenium/TestCases/StaticTests/Login.cs
@@ -1,22 +1,34 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
 using System.Threading.Tasks;
-using WebAutomation.Drivers.SeleniumWebDriverModel;
 using WebAutomation.Helpers;
+using WebAutomation.Helpers.PageObjs;
 
 namespace WebAutomation.WebTests.Selenium.TestCases.StaticTests
 {
-    public class Login
+    [TestFixture(typeof(FirefoxDriver))]
+    [TestFixture(typeof(ChromeDriver))]
+    [Parallelizable(ParallelScope.Fixtures)]
+    [Author("Raymond Dasilva", "raymond.dasilva@outlook.com")]
+    public class Login<TWebDriver> where TWebDriver : IWebDriver, new()
     {
-        #region Properies
-
-        protected IWebDriver Driver = new ChromeDriver();
-        private HomePage Page = new HomePage();
-
-        #endregion Properies
-
+        private IWebDriver Driver;        
+        
         #region Test Case
+
+        [OneTimeSetUp]
+        public void CreateDriver()
+        {
+            if (typeof(TWebDriver).Name == "ChromeDriver")
+                Driver = new ChromeDriver();
+            
+            else
+                Driver = new TWebDriver();            
+        }
 
         /// <summary>
         /// Initializes Driver, and heads to desired Homepage for Testing
@@ -25,41 +37,15 @@ namespace WebAutomation.WebTests.Selenium.TestCases.StaticTests
         [SetUp]
         public async Task SetUpTestAsync()
         {
-            Page = new HomePage(Driver);
-            if (!await Page.BeforeTestAsync("https://www.affirm.com/").ConfigureAwait(false))
-                Assert.Fail("Failed going to Home Page...");
-
-            await Task.Delay(5000).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// Test: Attempt signing in through UI.
-        /// </summary>
-        /// <returns></returns>
-        [Test, Order(1)]
-        public async Task CustomerSignInTestAsync()
-        {
-            // Wait until elements are visible then click it
-            ElementUtility.GetElement(Driver, ElementUtility.LocationTag.CssSelector, "#gatsby-focus-wrapper > div.Header-outer--NMN0k > div > div > div.Container-container--3LGAe.Header-wrapper--rEEKq.Header-hideOnSmartphone--3LTpT > a:nth-child(4) > span", 60).Click();
-            await Task.Delay(3000);
-
-            // Wait for Mobile Sign-in number to be present -- Then send random mobile number
-            string mobileSignInNum = "1234567890";
-            ElementUtility.GetElement(Driver, ElementUtility.LocationTag.CssSelector, "#Identity-container > div > div > div.pro1M70H9P9.proqW_lcgAc > form > div.pro1M70H9P9.pro1zXd09Vc > div > div > input", 60).Click();
-            Driver.FindElement(By.CssSelector("#Identity-container > div > div > div.pro1M70H9P9.proqW_lcgAc > form > div.pro1M70H9P9.pro1zXd09Vc > div > div > input")).SendKeys(mobileSignInNum);
-
-            // Click Continue -- Make sure it's valid and we don't have a popup
-            ElementUtility.GetElement(Driver, ElementUtility.LocationTag.CssSelector, "#Identity-container > div > div > div.pro1M70H9P9.proqW_lcgAc > form > div:nth-child(3) > button", 60).Click();
-            await Task.Delay(1000);
-
-            if (ElementUtility.GetElement(Driver, ElementUtility.LocationTag.CssSelector, "#Identity-container > div > div > div.pro1M70H9P9.proqW_lcgAc > div.pro3PQmJOfN.proPEJz23Bn > strong > p", 60).Displayed)
-                Assert.Fail("Looks like we don't have a valid test sign-in Number.");
-
-            // We just texted you Popup -- continue with Login Auth.
-            // TODO
-
-            // Test Passed.
-            Assert.Pass();
+            try 
+            {
+                var Page = new HomePage(Driver);
+                await Page.GoToHomePage().ConfigureAwait(false);
+            }
+            catch (Exception exception)
+            {
+                Assert.Fail($"Failed going to Home Page... Error: {exception.Message}");
+            } 
         }
 
         /// <summary>
@@ -69,9 +55,39 @@ namespace WebAutomation.WebTests.Selenium.TestCases.StaticTests
         [TearDown]
         public async Task TearDownTestAsync()
         {
-            Page.TearDown();
-            await Task.Delay(300);
+            await ElementUtility.TearDown(Driver);
         }
+
+        /// <summary>
+        /// Test: Attempt signing in through UI.
+        /// </summary>
+        /// <returns></returns>
+        [Test, Order(1)]
+        public async Task CustomerSignInTestAsync()
+        {
+
+            // Wait until elements are visible then click it
+            ElementUtility.GetElement(Driver, By.CssSelector("#gatsby-focus-wrapper > div.Header-outer--NMN0k > div > div > div.Container-container--3LGAe.Header-wrapper--rEEKq.Header-hideOnSmartphone--3LTpT > a:nth-child(4) > span"), 60).Click();
+            await Task.Delay(3000);
+
+            // Wait for Mobile Sign-in number to be present -- Then send random mobile number
+            string mobileSignInNum = "1234567890";
+            var mobileNumberBox = ElementUtility.GetElement(Driver, By.CssSelector("#Identity-container > div > div > div.pro1M70H9P9.proqW_lcgAc > form > div.pro1M70H9P9.pro1zXd09Vc > div > div > input"), 60);
+            mobileNumberBox.SendKeys(mobileSignInNum);            
+
+            // Click Continue -- Make sure it's valid and we don't have a popup
+            ElementUtility.GetElement(Driver, By.CssSelector("#Identity-container > div > div > div.pro1M70H9P9.proqW_lcgAc > form > div:nth-child(3) > button"), 60).Click();
+            await Task.Delay(1000);
+
+            if (ElementUtility.GetElement(Driver, By.CssSelector("#Identity-container > div > div > div.pro1M70H9P9.proqW_lcgAc > div.pro3PQmJOfN.proPEJz23Bn > strong > p"), 60).Displayed)
+                Assert.Fail("Looks like we don't have a valid test sign-in Number.");
+
+            // We just texted you Popup -- continue with Login Auth.
+            // TODO
+
+            // Test Passed.
+            Assert.Pass();
+        }        
 
         #endregion Test Case
     }
